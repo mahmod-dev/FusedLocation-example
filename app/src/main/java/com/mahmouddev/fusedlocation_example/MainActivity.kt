475bd795c1a43f9573c5f44b23234e9c7ad95a24
Mahmoud-Dev <mahmod-dev@outlook.com>
2020-10-22 18:13:40 +0300

    get Address based on (lat&lng)

--- app/src/main/java/com/mahmouddev/fusedlocation_example/MainActivity.kt
+++ app/src/main/java/com/mahmouddev/fusedlocation_example/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.mahmouddev.fusedlocation_example
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.location.Address
+import android.location.Geocoder
 import android.location.Location
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.mahmouddev.fusedlocation_example.fusedlocation.GPS_REQUEST_CODE
 import com.mahmouddev.fusedlocation_example.fusedlocation.LocationHelper
 import com.mahmouddev.fusedlocation_example.fusedlocation.LocationManager
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity"
@@ -21,7 +25,7 @@ class MainActivity : AppCompatActivity() {
         initGpsLocation()
     }
 
-
+    @SuppressLint("SetTextI18n")
     private fun initGpsLocation() {
         locationHelper = LocationHelper(this, object : LocationManager {
 
@@ -32,6 +36,7 @@ class MainActivity : AppCompatActivity() {
                 Log.e(TAG, "onLocationChanged latitude: ${location?.latitude}")
                 Log.e(TAG, "onLocationChanged longitude: ${location?.longitude}")
                 tvLatLng.text = " latitude: $lat \n longitude: $lng"
+                tvAddress.text = getAddressDetails(lat!!, lng!!)
 
             }
 
@@ -42,6 +47,7 @@ class MainActivity : AppCompatActivity() {
                 lat = location?.latitude
                 lng = location?.longitude
                 tvLatLng.text = " latitude: $lat \n longitude: $lng"
+                tvAddress.text = getAddressDetails(lat!!, lng!!)
 
             }
 
@@ -71,6 +77,38 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == GPS_REQUEST_CODE) {
             initGpsLocation()
         }
+    }
+
+    private fun getAddressDetails(lat: Double, lng: Double): String? {
+        val geoCoder = Geocoder(applicationContext, Locale.getDefault())
+        val addresses: List<Address>
+        var fullAddress = ""
+        try {
+            addresses = geoCoder.getFromLocation(lat, lng, 1)
+            if (addresses.isNotEmpty()) {
+                val address = addresses[0].getAddressLine(0)
+                val locality = addresses[0].locality
+                val subLocality = addresses[0].subLocality
+                val state = addresses[0].adminArea
+                val country = addresses[0].countryName
+                val postalCode = addresses[0].postalCode
+                val knownName = addresses[0].featureName
+                fullAddress =
+                    "address: $address \n locality: $locality \n subLocality: $subLocality \n" +
+                            "state: $state \n country: $country \n postalCode: $postalCode \n knownName: $knownName  "
+
+                Log.e(TAG, "getAddressDetails: $fullAddress")
+            } else
+                fullAddress = " address unavailable in your country! "
+
+        } catch (e: Exception) {
+            e.printStackTrace()
+            Log.e(TAG, "getAddressDetails Exception: ${e.message}")
+            return null
+        }
+        return fullAddress
+
+
     }
 
 
